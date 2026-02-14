0cbe1e819d431128eb2fe65e57568ac79830dbf3
Zainab Hassan <zainabmosunmola@gmail.com>
2026-02-14 04:06:40 -0600

    infra: add Terraform outputs for RDS endpoint and port

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "rds_endpoint" {
+  description = "RDS instance endpoint address"
+  value       = aws_db_instance.postgres.address
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.postgres.port
+}
