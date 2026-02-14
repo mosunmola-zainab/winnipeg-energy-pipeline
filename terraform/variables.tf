cec862ae59cb9430cf8877b41013f155a47238c9
Zainab Hassan <zainabmosunmola@gmail.com>
2026-02-14 03:47:38 -0600

    infra: adding Terraform variables for AWS region, project, and DB configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region for resource deployment"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "winnipeg-energy"
+}
+
+variable "db_name" {
+  description = "Name of the database"
+  type        = string
+  default     = "energy"
+}
+
+variable "db_username" {
+  description = "Database master username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Database master password"
+  type        = string
+  sensitive   = true
+}
